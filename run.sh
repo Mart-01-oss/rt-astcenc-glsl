3745fab33b25d35c9fdc49be2e7381f9a2fcbeb0
Mart <remartrobles09@gmail.com>
2025-11-11 10:45:30 +0800

    Update run.sh to set WGPU_LIB_PATH and fix command

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
+export WGPU_LIB_PATH=/data/data/com.termux/files/home/rt-astcenc-glsl/wgpu-native/lib/libwgpu_native.so
 glslangValidator -V -Os astc_compress.comp -o astc_compress.spv
-uv run python astc.py
+python astc.py
